fc807f1a68a1caa6fbb9b3178590480a5b1dc3ef
Caroline Davis <caroline.davis@mantelgroup.com.au>
2020-01-06 16:17:52 +1100

    fix: Refactor reducer into a switch statement

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,26 +3,24 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            // could do the ...state but as there is only 1 property, we can just remake an instance of it.
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + 5
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - 5
-        }
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT': 
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT': 
+            return {
+                counter: state.counter - action.val
+            }
     }
     return state;
 };
